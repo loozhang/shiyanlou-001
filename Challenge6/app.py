c79f0bfbd7b688e81d0d152a65086f7848c9646e
loozhang <loozhang@163.com>
2018-01-28 16:54:47 +0800

    update app.py

--- Challenge6/app.py
+++ Challenge6/app.py
@@ -12,4 +12,22 @@ def not_found(error):
 	return render_template('404.html'),404
 
 @app.route('/')
-def
+def index():
+	filelist=os.listdir('../files/')
+	return render_template('index.html',filelist=filelist)
+
+@app.route('/files/<filename>')
+def file(filename):
+	if filename=='helloshiyanlou':
+		with open('../files/helloshiyanlou.json','r') as file:
+			shiyanlou=json.loads(file.read())
+		return render_template('file.html',shiyanlou=shiyanlou)
+	elif filename=='helloworld':
+		with open('../files/helloworld.json','r') as file:
+			shiyanlou=json.loads(file.read())
+		return render_template('file.html',shiyanlou=shiyanlou)
+	else:
+		return render_template('404.html'),404
+
+if __name__=='__main__':
+	app.run()	
