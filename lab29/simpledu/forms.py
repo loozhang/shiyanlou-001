5d95d2a24565422ddb7a93727cb1f4f056726ff1
loozhang <loozhang@163.com>
2018-02-11 17:14:46 +0800

    add lab29

---
+++ lab29/simpledu/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import Length,Email,EqualTo,Required
+
+class RegisterForm(FlaskForm):
+	username=StringField('Username',validators=[Required(),Length(3,24)])
+	email=StringField('Email',validators=[Required(),Email()])
+	password=PasswordField('Password',validators=[Required(),Length(6,24)])
+	repeat_password=PasswordField('Repeat Password',validators=[Required(),EqualTo('password')])
+	submit=SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+	email=StringField('Email',validators=[Required(),Email()])
+	password=PasswordField('Password',validators=[Required(),Length(6,24)])
+	remember_me=BooleanField('Remember Me')
+	submit=SubmitField('Submit')
